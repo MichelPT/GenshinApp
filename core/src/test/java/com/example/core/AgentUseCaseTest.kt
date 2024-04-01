b876e84eb22e6a2878936df5d0c4ddf1e43fa903
MichelPT <michelpt76767@gmail.com>
2024-04-01 21:30:58 +0700

    success test

--- core/src/test/java/com/example/core/AgentUseCaseTest.kt
+++ core/src/test/java/com/example/core/AgentUseCaseTest.kt
@@ -1,37 +1,117 @@
 package com.example.core
 
-import androidx.appcompat.widget.ResourceManagerInternal
 import com.example.core.data.AgentRepository
-import com.example.core.data.source.local.entity.AgentEntity
+import com.example.core.data.Resource
+import com.example.core.domain.model.Agent
 import com.example.core.domain.usecase.AgentInteractor
-import com.example.core.domain.usecase.AgentUseCase
+import io.reactivex.Single
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Test
-import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.Mockito.`when`
-import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.MockitoAnnotations
 
 
-@RunWith(MockitoJUnitRunner::class)
 class AgentUseCaseTest {
-    private lateinit var agentUseCase: AgentUseCase
-
     @Mock
-    private lateinit var agentRepository: AgentRepository
+    lateinit var agentRepository: AgentRepository
+
+    val agentUseCase by lazy { AgentInteractor(agentRepository) }
 
     @Before
     fun setUp() {
-        agentUseCase = AgentInteractor(agentRepository)
-        val dummyAgent = AgentEntity(
-            role = "Just a student",
-            uuid = "1234567890",
-            fullPortrait = "https://example.com/portrait.jpg",
-            description = "Agent description",
-            displayIcon = "https://example.com/icon.png",
-            displayName = "Michel"
-        )
-        `when`(agentRepository.getAllAgents())
+        MockitoAnnotations.initMocks(this)
     }
-
+    @Test
+    fun `test get All agents`() {
+        `when` (agentRepository.getAllAgents()).thenReturn(
+                flowOf(
+                    Resource.Success(
+                        listOf(
+                            Agent(
+                                role = "student with a passion for coding",
+                                uuid = "1234567890",
+                                fullPortrait = "https://example.com/portrait.jpg",
+                                description = "Agent description",
+                                displayIcon = "https://example.com/icon.png",
+                                displayName = "Michel",
+                                isFavorite = false
+                            )
+                        )
+                    )
+                )
+            )
+        runBlocking {
+            agentUseCase.getAllAgents().first().data?.get(0)?.let{
+                assert(it.displayName == "Michel")
+                assert(it.role == "student with a passion for coding")
+                assert(it.description == "Agent description")
+                assert(it.displayIcon == "https://example.com/icon.png")
+                assert(it.fullPortrait == "https://example.com/portrait.jpg")
+                assert(!it.isFavorite)
+                assert(it.uuid == "1234567890")
+            }
+        }
+    }
 }
+//@RunWith(MockitoJUnitRunner::class)
+//class AgentUseCaseTest {
+//    private lateinit var agentUseCase: AgentUseCase
+//
+//    private val dummyAgent =
+//            Agent(
+//                role = "student with a passion for coding",
+//                uuid = "1234567890",
+//                fullPortrait = "https://example.com/portrait.jpg",
+//                description = "Agent description",
+//                displayIcon = "https://example.com/icon.png",
+//                displayName = "Michel",
+//                isFavorite = false
+//            )
+//
+//    @Mock
+//    private lateinit var agentRepository: AgentRepository
+//
+//    @Before
+//    fun setUp() {
+//        agentUseCase = AgentInteractor(agentRepository)
+//        `when`(agentUseCase.getAllAgents()).thenReturn(
+//                flow {
+//                    Resource.Success(
+//                        listOf(
+//                            Agent(
+//                                role = "student with a passion for coding",
+//                                uuid = "1234567890",
+//                                fullPortrait = "https://example.com/portrait.jpg",
+//                                description = "Agent description",
+//                                displayIcon = "https://example.com/icon.png",
+//                                displayName = "Michel",
+//                                isFavorite = false
+//                            )
+//                        )
+//                    )
+//                }
+//            )
+//    }
+//
+//
+//
+//
+//
+//    @Test
+//    fun `Get all agents`() {
+//        var agents: Agent? = null
+//        val agentsFlow = agentUseCase.getAllAgents()
+//        runBlocking {
+//            val agent = agentsFlow.first().data
+//            agents = agent?.get(0)
+//        }
+//
+//        verify(agentRepository).getAllAgents()
+//        verifyNoMoreInteractions(agentRepository)
+//        assertEquals(dummyAgent, agents)
+//    }
+//
+//}
