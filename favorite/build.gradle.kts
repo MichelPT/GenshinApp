c3a41caf6629924f23e23707bc30a1dbeef06cb4
MichelPT <michelpt76767@gmail.com>
2024-04-03 15:54:31 +0700

    cleaned up code

--- favorite/build.gradle.kts
+++ favorite/build.gradle.kts
@@ -23,13 +23,6 @@ android {
                 "proguard-rules.pro"
             )
         }
-        debug {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
     }
 
     kotlinOptions {
